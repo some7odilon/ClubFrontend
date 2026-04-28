177eb58690f6786009da7ee7e76d9ecea6244db9
some7odilon <somebodilon@gmail.com>
2026-04-28 15:35:01 +0000

    Fix: Correction dans api.ts

--- app/API.ts
+++ app/API.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_URL + "API/"
+    baseURL:process.env.NEXT_PUBLIC_API_URL 
 })
 
 export default api
